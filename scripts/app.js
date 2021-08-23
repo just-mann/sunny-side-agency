c3b9a690a6df09a5a2c39c0eb2e0d928f0c2fcf2
Justin <justinkollie890@yahoo.com>
2021-08-23 14:41:39 -0400

    Styled the menu click
    Changes made

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
 console.log('It works');
+
+
+let menuToggle = document.getElementById('menu-toggle');
+let menu = document.getElementById('menu');
+menuToggle.onclick = function() {
+  menu.classList.add('open-menu');
