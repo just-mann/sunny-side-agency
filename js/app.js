07329e699d5c5c3ccb6d2aaa145ec964a9431703
Justin <64009695+just-mann@users.noreply.github.com>
2021-08-24 23:38:34 -0400

    Changes made in javascript

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ console.log('It works');
 
 let menuToggle = document.getElementById('menu-toggle');
 let menu = document.getElementById('menu');
+let desktopSize = 1440;
+
+
+
 
 menuToggle.addEventListener('click', () => {
   if (menu.classList.contains('open-menu')) {
@@ -11,3 +15,11 @@ menuToggle.addEventListener('click', () => {
     menu.classList.add('open-menu');
   }
 });
+
+const removeMenu = () => {
+  if (window.innerWidth >= desktopSize) {
+    menu.classList.remove('open-menu');
+  }
+}
+
+window.onresize = removeMenu;
